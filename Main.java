b9d96fac23c55315a130f0cc1842416cdec1fdd5
Leandro <you@example.com>
2022-10-12 11:50:11 -0300

    algumas mudanças no main e personagem

--- Main.java
+++ Main.java
@@ -10,6 +10,11 @@ public class Main {
                 char rf1 = sc.next().charAt(0);
                 sc.nextLine();
                 if (rf1 == 'i'){
+                        Personagem[] personagems = new Personagem[3];
+                        personagems[0] = new Arqueiro();
+                        personagems[1] = new Guerreiro();
+                        personagems[2] = new Mago();
+
                         System.out.println("Digite c para criar um novo personagem, i para começar a partida ou s para sair do jogo");
                         char rf2 = sc.next().charAt(0);
                         sc.nextLine();
@@ -17,26 +22,57 @@ public class Main {
                                 System.out.println("Digite a para criar um Arqueiro, g para Guerreiro ou m para Mago");
                                 char rf21 = sc.next().charAt(0);
                                 sc.nextLine();
+                                System.out.println("Escolha um nome para seu personagem");
+                                String nomePersonagem = sc.nextLine();
+
                                 if (rf21 == 'a'){
-                                        System.out.println("Escolha um nome para seu personagem");
-                                        String nomePersonagem = sc.nextLine();
-                                        Arqueiro arqueiro = new Arqueiro();
-                                        arqueiro.setNomePersonagem(nomePersonagem);
+
+                                        personagems[0].setNomePersonagem(nomePersonagem);
                                         System.out.println("Escolha sua arma, 1 Balestra ou 2 ArcoLongo ");
                                         int armaPersonagem = sc.nextInt();
-                                        if(armaPersonagem == 1){
-                                                arqueiro.setArmaPersonagem(new Balestra());
-                                        }if(armaPersonagem == 2){
-                                                arqueiro.setArmaPersonagem(new ArcoLongo());
-                                        }
 
+                                if(armaPersonagem == 1){
+                                        personagems[0].setArmaPersonagem(new Balestra());
+                                }{
+                                        personagems[0].setArmaPersonagem(new ArcoLongo());
+                                }
+                                        System.out.println(personagems[0].getNomePersonagem());
+                                        System.out.println(personagems[0].getArmaPersonagem());
+                                        System.out.println(personagems[0].getAtaque());
+                                        System.out.println(personagems[0].getDefesa());
+                                }
+                                if (rf21 == 'g'){
+
+                                        personagems[1].setNomePersonagem(nomePersonagem);
+                                        System.out.println("Escolha sua arma, 1 Espada ou 2 Machado ");
+                                        int armaPersonagem = sc.nextInt();
+                                                if(armaPersonagem == 1){
+                                                        personagems[1].setArmaPersonagem(new Espada());
+                                                }{
+                                                        personagems[1].setArmaPersonagem(new Machado());
+                                                }
+                                                System.out.println(personagems[1].getNomePersonagem());
+                                                System.out.println(personagems[1].getArmaPersonagem());
+                                                System.out.println(personagems[1].getAtaque());
+                                                System.out.println(personagems[1].getDefesa());
+                                }
+                                if (rf21 == 'm'){
+                                        personagems[2].setNomePersonagem(nomePersonagem);
+                                        System.out.println("Escolha sua arma, 1 Varinha ou 2 Cajado ");
+                                        int armaPersonagem = sc.nextInt();
+                                        if(armaPersonagem == 1){
+                                                personagems[2].setArmaPersonagem(new Varinha());
+                                        }{
+                                                personagems[2].setArmaPersonagem(new Cajado());
+                                        }
+                                        System.out.println(personagems[2].getNomePersonagem());
+                                        System.out.println(personagems[2].getArmaPersonagem());
+                                        System.out.println(personagems[2].getAtaque());
+                                        System.out.println(personagems[2].getDefesa());
 
                                 }
                         }
-
-
                 }
-
 
 
 
