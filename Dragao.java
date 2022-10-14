6a51cd2d2edfed89e0dee57c47122aa508e73058
FabricioFLS <fabricioluizsouza@gmail.com>
2022-10-13 22:16:09 -0300

    ImplementacaoTela

--- Dragao.java
+++ Dragao.java
@@ -1,7 +1,10 @@
-public class Dragao {
+public class Dragao extends Personagem{
 
     public Dragao() {
         super();
+        setNomePersonagem("Lazyprog");
+        setAtaque(10);
+        setDefesa(15);
     }
     
 }
